6a754a54a81e26f48fa33c273dd9084da7717019
Slawomir Nowodworski <harpslav@gmail.com>
2018-06-16 23:20:19 +0200

    JS basics 1 i 2. Funkcje alert i console.log - wpisywanie nazwiska i wieku

---
+++ js-basics-02/script.js
@@ -0,0 +1,9 @@
+var firstName = prompt("What's your name?");
+var lastName = prompt("What's your surname?");
+var age = prompt("What's your age?");
+var fullName = firstName + " " + lastName;
+
+console.log("Hello! Your name is " + fullName + "."); 
+console.log("You are " + age + " years old.");
+alert("Hello! Your name is " + fullName + "." + " You are " + age + " years old.");
+
